4c906d7f1cf395afb56b3e2ada4daecf2007fa90
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-18 19:44:47 -0700

    Add project files.

---
+++ BFSGraph/Graph.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFSGraph
+{
+    class Graph
+    {
+        List<List<int>> adj = new List<List<int>>();
+        int Nodes;
+
+        public Graph(int N)
+        {
+            this.Nodes = N;
+            for (int i = 0; i < N; i++)
+            {
+                adj.Add(new List<int>());
+            }
+        }
+
+        public void AddEdge(int Source, int Destination)
+        {
+            adj[Source].Add(Destination);
+        }
+
+        public void BFS(int StartNode)
+        {
+            bool[] visited = new bool[Nodes];
+            Queue<int> Q = new Queue<int>();
+            Q.Enqueue(StartNode);
+            Console.Write(StartNode);
+
+            while(Q.Count != 0)
+            {
+                Console.WriteLine();
+                int Current = Q.Dequeue();
+
+                foreach (var child in adj[Current])
+                {
+                    if(!visited[child])
+                    {
+                        Console.Write(child);
+                        visited[child] = true;
+                        Q.Enqueue(child);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ BFSGraph/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BFSGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph G = new Graph(5);
+
+            G.AddEdge(0, 1);
+            G.AddEdge(0, 2);
+            G.AddEdge(1, 3);
+            G.AddEdge(2, 4);
+            G.AddEdge(3, 4);
+
+            G.BFS(0);
+            Console.ReadLine();
+        }
+    }
+}
